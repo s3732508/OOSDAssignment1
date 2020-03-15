284477c6f01955c6a448b5ba23df37623964ecf8
s3732508 <s3732508@student.rmit.edu.au>
2020-03-16 03:45:59 +1100

    initial commit

---
+++ src/application/Board.java
@@ -0,0 +1,90 @@
+package application;
+
+
+
+import javafx.application.Application;
+import javafx.scene.Group;
+
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+public class Board extends Application {
+	Cell[][] map=new Cell[5][5];
+	
+	@Override
+	public void start(Stage stage) {
+		Group root = new Group();
+		for (int row=0; row < 5; row++) {
+			
+			
+			switch (row) {
+			case 0:
+				for (int x = 0; x < 3; x++) {
+					
+					map[x][row] = new Cell(x, row);
+					
+					root.getChildren().add(map[x][row].cell);
+					
+				}
+				break;
+			case 1:
+				for (int x = 0; x < 4; x++) {
+					
+					map[x][row] = new Cell(x, row);
+					root.getChildren().add(map[x][row].cell);
+				}
+				break;
+			case 2:
+				for (int x = 0; x < 5; x++) {
+					
+					map[x][row] = new Cell(x, row);
+					root.getChildren().add(map[x][row].cell);
+				}
+				break;
+			case 3:
+				for (int x = 1; x < 5; x++) {
+					
+					map[x][row] = new Cell(x, row);
+					root.getChildren().add(map[x][row].cell);
+				}
+				break;
+			case 4:
+				for (int x = 2; x < 5; x++) {
+					
+					map[x][row] = new Cell(x, row);
+					root.getChildren().add(map[x][row].cell);
+					
+				}
+				break;
+
+			}
+		}
+		
+		
+		
+		for(int k=0;k<5;k++) {
+			
+			map[k][k].setOccupied();
+			
+		}
+		
+		
+
+		// Creating a scene object
+		Scene scene = new Scene(root, 600, 300);
+
+		// Setting title to the Stage
+		stage.setTitle("Drawing a Polygon");
+
+		// Adding scene to the stage
+		stage.setScene(scene);
+
+		// Displaying the contents of the stage
+		stage.show();
+	}
+
+	public static void main(String args[]) {
+		launch(args);
+	}
+}
---
+++ src/application/Cell.java
@@ -0,0 +1,42 @@
+package application;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Polygon;
+
+public class Cell {
+	
+	Polygon cell = new Polygon();
+	int x;
+	int y;
+	boolean occupied = false;
+	
+	public  Cell(int x,int y) {
+		this.x=x;
+		this.y=y;
+		
+		double size=15;
+		 
+		cell.getPoints().addAll(new Double[]{ 
+				(6+(4*x-2*y))*size, (6+(3*y))*size,
+				(6+(4*x-2*y))*size, (4+(3*y))*size,
+				(8+(4*x-2*y))*size, (3+(3*y))*size,
+				(10+(4*x-2*y))*size, (4+(3*y))*size,
+				(10+(4*x-2*y))*size, (6+(3*y))*size,
+				(8+(4*x-2*y))*size, (7+(3*y))*size
+	      }); 
+		cell.setFill(Paint.valueOf("#F39C12"));
+	      cell.setStroke(Color.BLACK);
+	}
+	
+	public void setOccupied() {
+		this.occupied= true;
+		cell.setFill(Paint.valueOf("#F1C40F"));
+		
+		
+		
+		
+	}
+	
+	
+}
