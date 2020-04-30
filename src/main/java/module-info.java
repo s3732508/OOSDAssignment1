b7631adb17010da397629afddc0f294e92b0ba1e
Luke Shuster <luke.shuster@hotmail.com>
2020-05-01 03:01:16 +1000

    removed files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module com.sharknados {
     requires javafx.controls;
-    requires java.rmi;
-    requires java.desktop;
     exports com.sharknados.models;
     exports com.sharknados;
     exports com.sharknados.models.pieces.eagles;
